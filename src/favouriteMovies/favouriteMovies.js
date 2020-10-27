cb8a5ef7d52c1650a8448417b1c011078ce4db70
Mostafa_Farag14 <mostafafarag140@gmail.com>
2020-10-27 00:04:23 +0000

    first commit

---
+++ src/favouriteMovies/favouriteMovies.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FavouriteMovies = ({profiles, users, movies}) => {
+
+  const likedMovies = movies.map( movie => {
+		return({
+movie: movie.name,
+users: profiles.map( profile => users[])          
+})
+  
+return(<div>
+       {
+  		
+})
+		}
+       </div>
+)
+}
